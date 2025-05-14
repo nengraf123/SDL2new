6ba7188b34c23c98d0a32b85d61fbc0ce2223e5a
nengraf123 <nengraf123@gmail.com>
2025-05-15 02:08:29 +0300

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <SDL2/SDL.h>
 #include <iostream>
 
+
+
 int main(int argc, char* argv[]) {
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
         std::cerr << "Ошибка инициализации SDL: " << SDL_GetError() << std::endl;
@@ -33,26 +35,115 @@ int main(int argc, char* argv[]) {
     bool running = true;
     SDL_Event event;
 
-    SDL_Rect knopka1 {100, 100, 200, 200};
+    /* Наши кнопки */
+    SDL_Rect knopka1 {0, 0, 200, 75};
+
+
+    /* Невидимый прямоугольник-курсора */
+    SDL_Rect KvadratNaCursore = {0, 0, 2, 2};
 
     while (running) {
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {running = false;}}
+        while (SDL_PollEvent(&event)) {if (event.type == SDL_QUIT) {running = false;}}
+
+        
+        // Обновляем позицию «курсорного» прямоугольника
+        int mx, my;
+        SDL_GetMouseState(&mx, &my);
+        KvadratNaCursore.x = mx - KvadratNaCursore.w / 2;
+        KvadratNaCursore.y = my - KvadratNaCursore.h / 2;
+        /* Хуйня для Клавиатуры */
         const Uint8 *keyState = SDL_GetKeyboardState(NULL);
+        /* Фон */
+        SDL_SetRenderDrawColor(renderer, 123, 123, 123, 255); SDL_RenderClear(renderer);
+        /* Выход на Esc */
         if (keyState[SDL_SCANCODE_ESCAPE]) running = false;
 
 
-        /* Фон */
-        SDL_SetRenderDrawColor(renderer, 123, 123, 123, 255); SDL_RenderClear(renderer);
+        /* Проверяем пересечение */
+        bool hovering = SDL_HasIntersection(&KvadratNaCursore, &knopka1);
 
 
-        SDL_RenderPresent(renderer);
-        SDL_Delay(16); // ~60 FPS
-    }
 
+        /* Рисуем кнопку 1: цвет зависит от наведения */
+        if (hovering) SDL_SetRenderDrawColor(renderer, 170, 170, 255, 255);
+        else          SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+        SDL_RenderFillRect(renderer, &knopka1);
+        /* Черная обводка вокруг кнопки */
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);SDL_RenderDrawRect(renderer, &knopka1);
+
+        /* Mожно отрисовать курсорный прямоугольник для отладки */
+        SDL_SetRenderDrawColor(renderer, 255,255,0,128);
+        SDL_RenderFillRect(renderer, &KvadratNaCursore);
+
+        SDL_RenderPresent(renderer);SDL_Delay(16); /* 60 FPS */}
+    //Ероры
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
     return 0;
 }
+
+// #include <SDL2/SDL.h>
+// #include <iostream>
+
+// int main(int argc, char* argv[]) {
+//     SDL_Init(SDL_INIT_VIDEO);
+//     SDL_Window* window = SDL_CreateWindow("Simple Button",
+//         SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, 0);
+//     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+
+//     // Кнопка
+//     SDL_Rect knopka1 = {300, 250, 200, 100};
+//     // Невидимый прямоугольник-курсора (например, 10×10)
+//     SDL_Rect KvadratNaCursore = {0, 0, 2, 2};
+
+//     bool running = true;
+//     SDL_Event e;
+
+//     while (running) {
+//         // Обновляем позицию «курсорного» прямоугольника
+//         int mx, my;
+//         SDL_GetMouseState(&mx, &my);
+//         KvadratNaCursore.x = mx - KvadratNaCursore.w / 2;
+//         KvadratNaCursore.y = my - KvadratNaCursore.h / 2;
+
+//         // Проверяем пересечение
+//         bool hovering = SDL_HasIntersection(&KvadratNaCursore, &knopka1);
+
+//         while (SDL_PollEvent(&e)) {
+//             if (e.type == SDL_QUIT) {
+//                 running = false;
+//             }
+//             else if (e.type == SDL_MOUSEBUTTONUP &&
+//                      e.button.button == SDL_BUTTON_LEFT &&
+//                      hovering)
+//             {
+//                 std::cout << "Button clicked via cursorRect!\n";
+//             }
+//         }
+
+//         // Рендер фон
+//         SDL_SetRenderDrawColor(renderer, 50, 50, 50, 255);
+//         SDL_RenderClear(renderer);
+
+//         // Рисуем кнопку: цвет зависит от hovering
+//         if (hovering) SDL_SetRenderDrawColor(renderer, 170, 170, 255, 255);
+//         else          SDL_SetRenderDrawColor(renderer, 200, 200, 200, 255);
+//         SDL_RenderFillRect(renderer, &knopka1);
+//         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+//         SDL_RenderDrawRect(renderer, &knopka1);
+
+//         // (Опционально) можно отрисовать курсорный прямоугольник для отладки
+//         SDL_SetRenderDrawColor(renderer, 255,255,0,128);
+//         SDL_RenderFillRect(renderer, &KvadratNaCursore);
+
+//         SDL_RenderPresent(renderer);
+//         SDL_Delay(16);
+//     }
+
+//     SDL_DestroyRenderer(renderer);
+//     SDL_DestroyWindow(window);
+//     SDL_Quit();
+//     return 0;
+// }
 
