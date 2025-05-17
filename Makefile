ea3860d18ea8dfcce85464a5dcbe4a623b25cad8
nengraf123 <nengraf123@gmail.com>
2025-05-17 12:52:40 +0300

    update

--- Makefile
+++ Makefile
@@ -1,29 +1,3 @@
-CC := g++
-CFLAGS := -Isrc/biblioteki/SDL2 -Isrc/biblioteki -std=c++17 -Wall
-TINY_CFLAGS := $(CFLAGS) -Wno-format-overflow
-LDFLAGS := -Lsrc/biblioteki/SDL2 -lSDL2 -lSDL2_image -lSDL2_ttf -lSDL2_mixer
-OBJDIR := src/file_o
-BINDIR := bin
-SRCS := src/main.cpp src/common/common.cpp src/mainMenu/mainMenu.cpp src/musicPlayer/musicPlayer.cpp src/settings/settings.cpp src/towerDefens/towerDefens.cpp
-TINY_SRC := src/biblioteki/tinyfiledialogs.c
-OBJS := $(SRCS:%.cpp=$(OBJDIR)/%.o) $(OBJDIR)/src/biblioteki/tinyfiledialogs.o
-TARGET := $(BINDIR)/app
-
-all: $(TARGET)
-
-$(TARGET): $(OBJS)
-	@mkdir -p $(BINDIR)
-	$(CC) $(OBJS) -o $(TARGET) $(LDFLAGS)
-
-$(OBJDIR)/%.o: %.cpp
-	@mkdir -p $(dir $@)
-	$(CC) $(CFLAGS) -c $< -o $@
-
-$(OBJDIR)/src/biblioteki/tinyfiledialogs.o: $(TINY_SRC)
-	@mkdir -p $(dir $@)
-	$(CC) $(TINY_CFLAGS) -c $< -o $@
-
-clean:
-	rm -rf $(OBJDIR) $(BINDIR)
-
-.PHONY: all clean
+all:
+	g++ src/main.cpp -o bin/app -lSDL2
+	./app
--- src/main.cpp
+++ src/main.cpp
@@ -1,162 +1,78 @@
-#include "common/common.h"
-#include "mainMenu/mainMenu.h"
-#include "musicPlayer/musicPlayer.h"
-#include "settings/settings.h"
-#include "towerDefens/towerDefens.h"
-#include <filesystem>
-#include <fstream>
+#include <SDL2/SDL.h>
 #include <iostream>
 
-using namespace std;
-
-int main(int argc, char* argv[]) {
-    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0) {
-        cerr << "Ошибка инициализации SDL: " << SDL_GetError() << endl;
-        return 1;
-    }
-    if (IMG_Init(IMG_INIT_PNG) != IMG_INIT_PNG) {
-        cerr << "Ошибка инициализации SDL_image: " << IMG_GetError() << endl;
-        SDL_Quit();
-        return 1;
-    }
-    if (TTF_Init() < 0) {
-        cerr << "Ошибка инициализации SDL_ttf: " << TTF_GetError() << endl;
-        IMG_Quit();
-        SDL_Quit();
-        return 1;
-    }
-    if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0) {
-        cerr << "Ошибка инициализации SDL_mixer: " << Mix_GetError() << endl;
-        TTF_Quit();
-        IMG_Quit();
-        SDL_Quit();
-        return 1;
-    }
-
-    cerr << "Текущая рабочая директория: " << filesystem::current_path() << endl;
-
-    SDL_Window* window = SDL_CreateWindow("SDL2new", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 800, SDL_WINDOW_SHOWN);
-    if (!window) {
-        cerr << "Ошибка создания окна: " << SDL_GetError() << endl;
-        Mix_CloseAudio();
-        TTF_Quit();
-        IMG_Quit();
-        SDL_Quit();
-        return 1;
-    }
-
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    if (!renderer) {
-        cerr << "Ошибка создания рендера: " << SDL_GetError() << endl;
-        SDL_DestroyWindow(window);
-        Mix_CloseAudio();
-        TTF_Quit();
-        IMG_Quit();
-        SDL_Quit();
-        return 1;
-    }
-
-    TTF_Font* font = TTF_OpenFont("font/arial.ttf", 24);
-    if (!font) {
-        cerr << "Ошибка загрузки шрифта: " << TTF_GetError() << endl;
-        SDL_DestroyRenderer(renderer);
-        SDL_DestroyWindow(window);
-        Mix_CloseAudio();
-        TTF_Quit();
-        IMG_Quit();
-        SDL_Quit();
-        return 1;
-    }
-
-    TTF_Font* titleFont = TTF_OpenFont("font/arial.ttf", 48);
-    if (!titleFont) {
-        cerr << "Ошибка загрузки шрифта заголовка: " << TTF_GetError() << endl;
-        TTF_CloseFont(font);
-        SDL_DestroyRenderer(renderer);
-        SDL_DestroyWindow(window);
-        Mix_CloseAudio();
-        TTF_Quit();
-        IMG_Quit();
-        SDL_Quit();
-        return 1;
-    }
-
-    string musicPath = "music/muzika.mp3";
-    string folderPath;
-    ifstream inFile("save/music_path.txt");
-    if (inFile.is_open()) {
-        getline(inFile, musicPath);
-        getline(inFile, folderPath);
-        inFile.close();
-    }
-
-    MainMenu mainMenu;
-    mainMenu.init(renderer, titleFont);
-
-    MusicPlayer musicPlayer;
-    musicPlayer.init(musicPath, folderPath);
-
-    Settings settings;
-    settings.init(renderer, font);
-
-    TowerDefens towerDefens;
-    towerDefens.init();
-
-    int scene = -1; // Начать с главного меню
+class Game {
+private:
+    SDL_Window* window = nullptr;
+    SDL_Renderer* renderer = nullptr;
     bool running = true;
-    SDL_Event event;
-    int mx = 0, my = 0;
 
-    while (running) {
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
-                running = false;
-            } else if (event.type == SDL_MOUSEMOTION) {
-                mx = event.motion.x;
-                my = event.motion.y;
-            } else if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_ESCAPE) {
-                if (scene == -1) {
+public:
+    Game() {
+        // Инициализация SDL
+        if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+            std::cerr << "Ошибка инициализации SDL: " << SDL_GetError() << std::endl;
+            running = false;
+            return;
+        }
+
+        // Создание окна
+        window = SDL_CreateWindow(
+            "SDL2 C++ Пример",
+            SDL_WINDOWPOS_CENTERED,
+            SDL_WINDOWPOS_CENTERED,
+            800, 600,
+            SDL_WINDOW_SHOWN
+        );
+
+        if (!window) {
+            std::cerr << "Ошибка создания окна: " << SDL_GetError() << std::endl;
+            running = false;
+            return;
+        }
+
+        // Создание рендера
+        renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+        if (!renderer) {
+            std::cerr << "Ошибка создания рендера: " << SDL_GetError() << std::endl;
+            running = false;
+            return;
+        }
+    }
+
+    ~Game() {
+        // Очистка ресурсов
+        if (renderer) SDL_DestroyRenderer(renderer);
+        if (window) SDL_DestroyWindow(window);
+        SDL_Quit();
+    }
+
+    void run() {
+        if (!running) return;
+
+        SDL_Event event;
+        while (running) {
+            // Обработка событий
+            while (SDL_PollEvent(&event)) {
+                if (event.type == SDL_QUIT) {
                     running = false;
-                } else {
-                    scene = -1; // Вернуться в главное меню
                 }
             }
-            if (scene == -1) {
-                mainMenu.handleEvents(event, scene, running, mx, my);
-            } else if (scene == 0) {
-                musicPlayer.handleEvents(event, renderer, font, titleFont, mx, my, running, scene);
-            } else if (scene == 1) {
-                towerDefens.handleEvents(event, mx, my, scene);
-            } else if (scene == 2) {
-                settings.handleEvents(event, scene, mx, my);
-            }
+
+            // Очистка экрана (черный цвет)
+            SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+            SDL_RenderClear(renderer);
+
+            // Здесь можно добавить отрисовку
+
+            // Обновление экрана
+            SDL_RenderPresent(renderer);
         }
-
-        SDL_SetRenderDrawColor(renderer, 30, 30, 30, 255); // Тёмный фон
-        SDL_RenderClear(renderer);
-
-        if (scene == -1) {
-            mainMenu.render(renderer, titleFont, mx, my);
-        } else if (scene == 0) {
-            musicPlayer.update(renderer, font);
-            musicPlayer.render(renderer, font, mx, my);
-        } else if (scene == 1) {
-            towerDefens.update(renderer, font);
-            towerDefens.render(renderer, font, mx, my);
-        } else if (scene == 2) {
-            settings.render(renderer, font, mx, my);
-        }
-
-        SDL_RenderPresent(renderer);
     }
+};
 
-    TTF_CloseFont(font);
-    TTF_CloseFont(titleFont);
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    Mix_CloseAudio();
-    TTF_Quit();
-    IMG_Quit();
-    SDL_Quit();
+int main(int argc, char* argv[]) {
+    Game game;
+    game.run();
     return 0;
 }
