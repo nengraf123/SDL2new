3f7637a243f3549718561abcfb5e92b80c41632c
nengraf123 <nengraf123@gmail.com>
2025-05-19 20:53:46 +0300

    update

--- src/h/error1.h
+++ src/h/error1.h
@@ -1,8 +1,9 @@
+    // для компиляции на wayland
+    SDL_SetHint(SDL_HINT_VIDEODRIVER, "wayland");
+
     // Инициализация SDL
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         printf("Ошибка инициализации SDL: %s\n", SDL_GetError());
         return 1;
     }
 
-    // для компиляции на wayland
-    SDL_SetHint(SDL_HINT_VIDEODRIVER, "wayland");
